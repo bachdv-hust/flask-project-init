30b70c59ce1d7475a45519e6065208ac6ac870bc
bachdv <bachdv.hust@gmail.com>
2021-11-29 07:35:29 +0700

    new

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,13 @@
 import os
-
 from flask import Flask
-def createApp ():
-    app = Flask(__name__)
-    # Create app blueprints
-    from .main import main as main_blueprint
-    app.register_blueprint(main_blueprint)
-    from .rating import rating as rating_blueprint
-    app.register_blueprint(rating_blueprint)
-    from .feedback import feedback as feedback_blueprint
-    app.register_blueprint(feedback_blueprint)
-    from .customer_support import support as support_blueprint
-    app.register_blueprint(support_blueprint)
-    return app
-app  = createApp()
+app = Flask(__name__)
+from .main import main as main_blueprint
+app.register_blueprint(main_blueprint)
+from .rating import rating as rating_blueprint
+app.register_blueprint(rating_blueprint)
+from .feedback import feedback as feedback_blueprint
+app.register_blueprint(feedback_blueprint)
+from .customer_support import support as support_blueprint
+app.register_blueprint(support_blueprint)
 if __name__ == '__main__':
     app.run(threaded=True, port=5000)
