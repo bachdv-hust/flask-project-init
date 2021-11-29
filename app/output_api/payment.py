0f8b7f7008c7f42690954c1f39371d8e4048da37
bachdv <bachdv.hust@gmail.com>
2021-11-29 17:13:45 +0700

    fafa

--- app/output_api/payment.py
+++ app/output_api/payment.py
@@ -4,24 +4,24 @@ from flask import Blueprint, request, jsonify
 # lấy thông tin sản phẩm muốn đổi
 
 
-# @output.route('/payment', methods=['GET'])
-# def getExchangeProduct():
-#     null = None
-#     res = {
-#     }
-#     res["status_code"] = 200
-#     res["data"] = [{
-#         "product_id": 1,
-#         "product_name": "Vay"
-#     },
-#         {
-#         "product_id": 2,
-#         "product_name": "Ao dai"
-#     },
-#         {
-#         "product_id": 3,
-#         "product_name": "Vay ngan"
-#     },
+@output.route('/payment', methods=['GET'])
+def getExchangeProduct():
+    null = None
+    res = {
+    }
+    res["status_code"] = 200
+    res["data"] = [{
+        "product_id": 1,
+        "product_name": "Vay"
+    },
+        {
+        "product_id": 2,
+        "product_name": "Ao dai"
+    },
+        {
+        "product_id": 3,
+        "product_name": "Vay ngan"
+    },
 
-#     ]
-#     return jsonify(res)
+    ]
+    return jsonify(res)
