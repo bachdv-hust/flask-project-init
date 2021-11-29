b28ca3e88e2a803193e6a176f0c498fefa592f02
bachdv <bachdv.hust@gmail.com>
2021-11-29 18:01:37 +0700

    dasd

--- app/output_api/__init__.py
+++ app/output_api/__init__.py
@@ -2,4 +2,4 @@ from flask import Blueprint
 
 output = Blueprint('output', __name__)
 
-# from app.output_api import account_management,payment,shipping,rating
+from app.output_api import account_management,payment,shipping,rating
